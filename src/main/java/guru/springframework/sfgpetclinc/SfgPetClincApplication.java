f0df04ad6f558f194e47ee16e4e7bd626feef1e9
freedom <freedom.chuks7@gmail.com>
2019-04-30 13:23:39 -0700

    initial project add

---
+++ src/main/java/guru/springframework/sfgpetclinc/SfgPetClincApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClincApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgpetclinc/SfgPetClincApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgPetClincApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
